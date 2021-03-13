57a608fc2f1ead2744c8e692f360ac3f776eeafc
xsun95 <xsun95@outlook.com>
2021-03-13 19:50:30 +0800

    Add about route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,8 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+
+    app.get("about") { req -> String in
+        return "About us."
+    }
 }
